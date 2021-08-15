89d2cb855e26f738de6bc063152221fdb19a8fd6
EugeSorgetti <eugesorgetti@gmail.com>
2021-08-15 14:50:52 -0300

    calculadora con js

---
+++ app.js
@@ -0,0 +1,43 @@
+const pad = document.querySelector('.calculator-pad');
+const calculation = document.getElementById('calculation');
+const clear = document.getElementById('clear');
+const resultDisplay = document.getElementById('result');
+
+
+pad.addEventListener('click', e => {
+    
+
+    switch(e.target.innerText) {
+        case 'AC': 
+            calculation.innerText = '';
+            resultDisplay.innerText = '';
+            break;
+        case 'del':
+            if(calculation.innerText) {
+                calculation.innerText = calculation.innerText.slice(0, -1);
+            }
+            break;
+        case '=':
+            try {
+                let result = eval(calculation.innerText);
+                resultDisplay.innerText = result;
+            } catch {
+                resultDisplay.innerText = "Error"
+            }
+            break;
+        default: calculation.innerText += e.target.innerText;
+    }
+
+})
+
+//Toggle btn
+
+const toggleBtn = document.getElementById('toggle-btn');
+const calcBody = document.querySelector('.calculator-body');
+
+toggleBtn.addEventListener('click', e => {
+    calcBody.classList.toggle('light');
+    calcBody.classList.toggle('dark');
+    toggleBtn.classList.toggle('light');
+    toggleBtn.classList.toggle('dark');
+})
